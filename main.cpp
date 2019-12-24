909ed5450ee2a608eaf9a876d21c57b798aec631
Jimmy Smith <jimmy@doomkeeper.com>
2019-12-24 14:49:20 -0800

    hello world, phaser

--- main.cpp
+++ main.cpp
@@ -1,25 +1,46 @@
 #include "portaudio.h"
 #include <stdio.h>
 
-int Callback( const void *input, void *output,
-    unsigned long frameCount,
-    const PaStreamCallbackTimeInfo* timeInfo,
-    PaStreamCallbackFlags statusFlags,
-    void *userData )
-{
-    return 0;
-}
-
-#define ERR_WRAP(mac_err) do { result = mac_err ; line = __LINE__ ; if ( result != paNoError ) goto error; } while(0)
-#define SAMPLE_RATE (44100)
-#define NUM_SECONDS (10)
-
 typedef struct
 {
     float left_phase;
     float right_phase;
 }   
 paTestData;
+/* This routine will be called by the PortAudio engine when audio is needed.
+   It may called at interrupt level on some machines so don't do anything
+   that could mess up the system like calling malloc() or free().
+*/ 
+static int patestCallback( const void *inputBuffer, void *outputBuffer,
+                           unsigned long framesPerBuffer,
+                           const PaStreamCallbackTimeInfo* timeInfo,
+                           PaStreamCallbackFlags statusFlags,
+                           void *userData )
+{
+    /* Cast data passed through stream to our structure. */
+    paTestData *data = (paTestData*)userData; 
+    float *out = (float*)outputBuffer;
+    unsigned int i;
+    (void) inputBuffer; /* Prevent unused variable warning. */
+    
+    for( i=0; i<framesPerBuffer; i++ )
+    {
+        *out++ = data->left_phase;  /* left */
+        *out++ = data->right_phase;  /* right */
+        /* Generate simple sawtooth phaser that ranges between -1.0 and 1.0. */
+        data->left_phase += 0.01f;
+        /* When signal reaches top, drop back down. */
+        if( data->left_phase >= 1.0f ) data->left_phase -= 2.0f;
+        /* higher pitch so we can distinguish left and right. */
+        data->right_phase += 0.03f;
+        if( data->right_phase >= 1.0f ) data->right_phase -= 2.0f;
+    }
+    return 0;
+}
+
+#define ERR_WRAP(mac_err) do { result = mac_err ; line = __LINE__ ; if ( result != paNoError ) goto error; } while(0)
+#define SAMPLE_RATE (44100)
+#define NUM_SECONDS (10)
 
 int main(int argc, char** argv)
 {
@@ -43,7 +64,7 @@ int main(int argc, char** argv)
                                                    paFramesPerBufferUnspecified, which
                                                    tells PortAudio to pick the best,
                                                    possibly changing, buffer size.*/
-                                Callback, /* this is your callback function */
+                                patestCallback, /* this is your callback function */
                                 &data )); /*This is a pointer that will be passed to
                                                    your callback*/
 
@@ -63,7 +84,7 @@ error:
             Pa_StopStream(stream);
 
         Pa_CloseStream(stream);
-        stream = 0;
+        stream = nullptr;
     }
     printf(  "PortAudio error: %s\n", Pa_GetErrorText( err ) );
     return -1;
