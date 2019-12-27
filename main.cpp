5a223e50e478ec44f7440aeb87777a8ee95fc91e
Jimmy Smith <jimmy@doomkeeper.com>
2019-12-26 17:47:56 -0800

    pretty print pattern data

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@
 #include <chrono>
 #include <string>
 #include <vector>
+#include <map>
+
+uint16_t findNoteFromFreq(uint16_t amigaFreq, int& note, int& oct, int& ft, int tolerance=1);
 
 struct Sample
 {
@@ -22,7 +25,7 @@ struct Sample
 };
 struct Note
 {
-    int note:11;        // 0-??  = 11 bits = 0-2048 should be plenty for your needs.
+    uint16_t note:11;  // 0-??  = 11 bits = 0-2048 should be plenty for your needs.
     uint8_t number:5;  // 0-31  = 5 bits
     uint8_t effect;    // 0-15  = 4 bits, but use 8 to keep things even
     uint8_t eparm;     // 0-255 = 8 bits
@@ -41,6 +44,7 @@ struct Mod
 
 template<typename T> T Max(T a,T b) { return a > b ? a : b; }
 template<typename T> T Min(T a,T b) { return a < b ? a : b; }
+template<typename T> T Abs(T v) { return v < 0 ? -v : v; }
 
 template<typename T>
 struct RingBuffer
@@ -324,6 +328,10 @@ bool loadMod(void* mem, size_t size)
        printf("order=%d -> %u\n", i, order);
     }
     printf("nPatterns=%d\n", gMod.nPatterns);
+    assert((uintptr_t)sampleInfoState - (uintptr_t)mem == 1080);
+
+    //- read 4 bytes, discard them (we are at position 1080 again, this is M.K. etc!)
+    sampleInfoState+=4;
 
     gMod.patternData = (Note*)malloc(64 * gMod.nChannels * sizeof(Note) * (gMod.nPatterns+1));
 
@@ -335,16 +343,36 @@ bool loadMod(void* mem, size_t size)
         {
             uint32_t packed = *patterns++;
             //store SAMPLE_NUMBER as    (byte0 AND 0F0h) + (byte2 SHR 4)
-            note->number = (packed & 0xf0) + ((packed >> (16+4)) & 0xff);
+            uint8_t byte0 = packed & 0xff;
+            uint8_t byte2 = (packed >> 16) & 0xff;
+            note->number = (byte0 & 0xf0) + (byte2 >> 4);
 
             //- store PERIOD_FREQUENCY as ((byte0 AND 0Fh) SHL 8) + byte1;
             uint32_t freq = ((packed & 0xf) << 8) + ((packed >> 8) & 0xff);
+            int oct, semi, ft;
+            note->note = findNoteFromFreq(freq, semi, oct, ft);
             
             //- store EFFECT_NUMBER as    byte2 AND 0Fh
             note->effect = (packed >> 16) & 0xf;
 
             //- store EFFECT_PARAMETER as byte 3
             note->eparm = (packed >> 24) & 0xff;
+            const char* notestr[] = {"C-", "C#", "D-", "D#","E-", "F-", "F#", "G-","G#","A-", "A#", "B-" };
+
+            if( j % gMod.nChannels == 0 )
+            {
+                printf("\n");
+                printf("%02x    ", j / 4);
+            }
+            // note.
+            if( note->note) printf("%s%d ", notestr[semi],oct);
+            else printf("... ");
+            // sample #
+            if( note->number ) printf("%02x ", note->number);
+            else printf(".. ");
+            // effect.
+            printf("%x%02x", note->effect, note->eparm);
+            printf("     ");
 
             note++;  
         }
@@ -454,4 +482,96 @@ error:
     }
     printf(  "PortAudio error: %s\n", Pa_GetErrorText( err ) );
     return -1;
+}
+
+//
+static uint16_t gNotes[12*3*16] = 
+    {
+        // Tuning -8
+        907,856,808,762,720,678,640,604,570,538,508,480
+        ,453,428,404,381,360,339,320,302,285,269,254,240
+        ,226,214,202,190,180,170,160,151,143,135,127,120
+        // Tuning -7
+        ,900,850,802,757,715,675,636,601,567,535,505,477
+        ,450,425,401,379,357,337,318,300,284,268,253,238
+        ,225,212,200,189,179,169,159,150,142,134,126,119
+        //; Tuning -6
+        ,894,844,796,752,709,670,632,597,563,532,502,474
+        ,447,422,398,376,355,335,316,298,282,266,251,237
+        ,223,211,199,188,177,167,158,149,141,133,125,118
+        // Tuning -5
+        ,887,838,791,746,704,665,628,592,559,528,498,470
+        ,444,419,395,373,352,332,314,296,280,264,249,235
+        ,222,209,198,187,176,166,157,148,140,132,125,118
+        // Tuning -4
+        ,881,832,785,741,699,660,623,588,555,524,494,467
+        ,441,416,392,370,350,330,312,294,278,262,247,233
+        ,220,208,196,185,175,165,156,147,139,131,123,117
+        // Tuning -3
+        ,875,826,779,736,694,655,619,584,551,520,491,463
+        ,437,413,390,368,347,328,309,292,276,260,245,232
+        ,219,206,195,184,174,164,155,146,138,130,123,116
+        // Tuning -2
+        ,868,820,774,730,689,651,614,580,547,516,487,460
+        ,434,410,387,365,345,325,307,290,274,258,244,230
+        ,217,205,193,183,172,163,154,145,137,129,122,115
+        // Tuning -1
+        ,862,814,768,725,684,646,610,575,543,513,484,457
+        ,431,407,384,363,342,323,305,288,272,256,242,228
+        ,216,203,192,181,171,161,152,144,136,128,121,114
+        // normal tuning.
+        ,856,808,762,720,678,640,604,570,538,508,480,453 //; C-1 to B-1
+        ,428,404,381,360,339,320,302,285,269,254,240,226 // ; C-2 to B-2
+        ,214,202,190,180,170,160,151,143,135,127,120,113 // ; C-3 to B-3
+        // Tuning 1
+        ,850,802,757,715,674,637,601,567,535,505,477,450 //; same as above
+        ,425,401,379,357,337,318,300,284,268,253,239,225 //; but with 
+        ,213,201,189,179,169,159,150,142,134,126,119,113 //; finetune +1
+        // Tuning 2
+        ,844,796,752,709,670,632,597,563,532,502,474,447 //; etc, 
+        ,422,398,376,355,335,316,298,282,266,251,237,224 //; finetune +2
+        ,211,199,188,177,167,158,149,141,133,125,118,112 //
+        // Tuning 3
+        ,838,791,746,704,665,628,592,559,528,498,470,444
+        ,419,395,373,352,332,314,296,280,264,249,235,222
+        ,209,198,187,176,166,157,148,140,132,125,118,111
+        // Tuning 4
+        ,832,785,741,699,660,623,588,555,524,495,467,441
+        ,416,392,370,350,330,312,294,278,262,247,233,220
+        ,208,196,185,175,165,156,147,139,131,124,117,110
+        // Tuning 5
+        ,826,779,736,694,655,619,584,551,520,491,463,437
+        ,413,390,368,347,328,309,292,276,260,245,232,219
+        ,206,195,184,174,164,155,146,138,130,123,116,109
+        // Tuning 6
+        ,820,774,730,689,651,614,580,547,516,487,460,434
+        ,410,387,365,345,325,307,290,274,258,244,230,217
+        ,205,193,183,172,163,154,145,137,129,122,115,109
+        // Tuning 7
+        ,814,768,725,684,646,610,575,543,513,484,457,431
+        ,407,384,363,342,323,305,288,272,256,242,228,216
+        ,204,192,181,171,161,152,144,136,128,121,114,108
+    };
+
+uint16_t findNoteFromFreq(uint16_t amigaFreq, int& note, int& oct, int& ft, int tolerance)
+{    
+    for(oct=0; oct < 3; ++oct)
+    {
+        for(note=0; note < 12; note++ )
+        {
+            for(ft=0; ft < 16; ft++)
+            {
+                uint16_t off = (12*3*ft)+(12*oct)+note;
+                uint16_t val = gNotes[off];
+                int diff = (int)amigaFreq-val;
+                if( Abs(diff) <= tolerance)
+                {
+                    oct+=3;
+                    ft -=8;
+                    return off;
+                }
+            }
+        }
+    }
+    return 0; // fail.
 }
