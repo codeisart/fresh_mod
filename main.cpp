9434029ce15b9cb98cfd9cf644c8554246bd5495
Jimmy Smith <jimmy@doomkeeper.com>
2019-12-28 01:01:21 -0800

    playing basic mod, not effects, very glitchy

--- main.cpp
+++ main.cpp
@@ -11,7 +11,61 @@
 #include <vector>
 #include <map>
 
+template<typename T> T Max(T a,T b) { return a > b ? a : b; }
+template<typename T> T Min(T a,T b) { return a < b ? a : b; }
+template<typename T> T Abs(T v) { return v < 0 ? -v : v; }
+template<typename T> T Clamp(T min, T max, T v) { return Min(Max(max,v),v); }
+
+#define ERR_WRAP(mac_err) do { result = mac_err ; line = __LINE__ ; if ( result != paNoError ) goto error; } while(0)
+#define NUM_SECONDS (60)
+
+static const char* notestr[] = {"C-", "C#", "D-", "D#","E-", "F-", "F#", "G-","G#","A-", "A#", "B-" };
 uint16_t findNoteFromFreq(uint16_t amigaFreq, int& note, int& oct, int& ft, int tolerance=1);
+extern const uint16_t gNotes[];
+#define SAMPLE_RATE (48000)
+
+static std::atomic<bool> gExit;
+static std::atomic<bool> gWake;
+static std::condition_variable gWakeCv;
+static std::mutex m;
+static const int kFrameSize = 700;// SAMPLE_RATE /20.f;
+
+template<typename T>
+struct RingBuffer
+{
+    int head, tail, size;
+    T* buff;
+    RingBuffer(RingBuffer&) = delete;
+    RingBuffer(RingBuffer&& rhs) : buff(rhs.buff), head(rhs.head), tail(rhs.tail), size(rhs.size) { rhs.buff = nullptr; }
+    RingBuffer(int InSize) : buff(new T[InSize]), head(0), tail(0), size(InSize) {}
+    ~RingBuffer() { delete[] buff; }
+    
+    int increment(int i) const { return (i+1)%size; }  
+    int decrement(int i) const { return (i-1+size)%size; }
+
+    bool push(const T& v) 
+    { 
+        int next = increment(head);
+        if( next == tail) return false; // overflow
+        buff[head] = v; 
+        head=next;
+        return true;
+    }  
+    bool pop(T& out) 
+    {
+         if(isEmpty()) return false;  // underflow
+         out = buff[tail]; 
+         tail=increment(tail); 
+         return true; 
+    }
+
+    int num() const { return head >= tail ? head-tail : (head+size)-tail; }
+    int space() const { return size-1 - num(); }
+    bool isEmpty() const { return head == tail; }
+    bool isFull() const { return increment(head) == tail; }
+};
+
+RingBuffer<float> gOutput(kFrameSize*2*2); // stereo double buffered
 
 struct Sample
 {
@@ -23,10 +77,88 @@ struct Sample
    int loop_length = 0; 
    int8_t* data = nullptr;
 };
+
+struct Channel
+{
+    float freq = 0.f;   // actual freq in hz to play sound.
+    float samplePos = 0; // float for now.
+    int freqOffset = 0; // offset in note table
+    int vol = 0;
+    int ft = 0;
+    Sample* sample = nullptr;
+    Sample* lastSample = nullptr;
+    //RingBuffer<float> output;
+    //std::vector<float> output;
+
+    Channel() {}
+
+    // playback
+    void setSample(Sample* s) 
+    {
+        lastSample = s;
+    } 
+
+    void play(int offset=0) { samplePos = offset; }
+    void setFineTune(int ftune) { ft = ftune; }
+    void setVol(int v) { vol = v; }
+    void setFreq(int foffset) 
+    { 
+        freq = getFreqHz(foffset,ft);
+    }
+
+    static float getFreqHz(int foffset, int finetune)
+    {
+        int ftoff = 0;//12*3*7;//finetune;
+        static uint32_t max = 12*3*16;
+        int offset = foffset + ftoff;
+        assert(offset < max);
+        assert(offset >= 0);
+        float amigaval = gNotes[offset];
+        assert(amigaval > 0);
+        return (float)7159090.5 / (amigaval * 2);
+    }
+
+    // render thread.
+    int makeAudio(float* dst, float dstSize, float sampleRate )
+    {
+        if(lastSample) sample = lastSample;
+        if(!sample) return 0;
+        if(!sample->data) return 0;
+        bool bLooping = sample->loop_length > 0;
+        int i = 0;
+        //int shouldWrite = Min(output.spaframeSize);
+
+        float endPos = bLooping ? sample->loop_start + sample->loop_length : sample->length;
+        float freq = getFreqHz(freqOffset, ft);
+        if( freq <=0.f ) freq = 16000;
+        float ratio = (float)freq / sampleRate;
+
+        for (; i < dstSize; samplePos += ratio, dst++, i++)
+        {
+            if (samplePos >= endPos)
+            {
+                // restart loop?
+                if (bLooping)
+                    samplePos = sample->loop_start;
+                else
+                    return i;
+            }
+
+            // src.
+            float val = ((float)sample->data[(int)samplePos] / 128.f);
+            float gain = (float)vol / 64.f;
+            *dst += val * gain;
+            //if( !.push(val)) break;
+            //if( !output.push(val)) break;
+        }
+        return i;
+    }    
+};
+
 struct Note
 {
-    uint16_t note:11;  // 0-??  = 11 bits = 0-2048 should be plenty for your needs.
-    uint8_t number:5;  // 0-31  = 5 bits
+    uint16_t noteOffset:11;  // 0-??  = 11 bits = 0-2048 should be plenty for your needs.
+    uint8_t sampleNumber:5;  // 0-31  = 5 bits
     uint8_t effect;    // 0-15  = 4 bits, but use 8 to keep things even
     uint8_t eparm;     // 0-255 = 8 bits
 };
@@ -39,33 +171,115 @@ struct Mod
     std::string name;
     std::vector<Sample> samples;
     std::vector<int> order;
+    std::vector<Channel> channels;
     Note* patternData = nullptr;
+    std::mutex cs;
+
+    //playback
+    int speed = 6; // default
+    int currentOrder = 0; // index into order table.
+    int currentRow = 0;
+    int currentTick = 0;
+
+    void tick();
+    void updateRow(); 
+    void updateEffects() {}
+
+    size_t makeAudio(RingBuffer<float>* stereoOutput, float sampleRate, int frameSize);
+
 } gMod;
 
-template<typename T> T Max(T a,T b) { return a > b ? a : b; }
-template<typename T> T Min(T a,T b) { return a < b ? a : b; }
-template<typename T> T Abs(T v) { return v < 0 ? -v : v; }
-
-template<typename T>
-struct RingBuffer
+void Mod::tick()
 {
-    int head, tail, size;
-    T* buff;
-    RingBuffer(int InSize) : buff(new T[InSize]), head(0), tail(0), size(InSize) {}
-    ~RingBuffer() { delete[] buff; }
-    int increment(int i, int amnt=1) { return (i+amnt)%size; }  
-    bool push(const T& v) 
-    { 
-        int next = increment(head);
-        if( next == tail) return false; // overflow
-        buff[head] = v; 
-        head=next;
-        return true;
-    }  
-    T pop() { T v = buff[tail]; tail=increment(tail); return v; }
-    int num() const { return head >= tail ? head-tail : (head+size)-tail; }
-    int space() const { return size - num(); }
-};
+    if( ++currentTick >= speed)
+    {
+        updateRow();
+        currentTick = 0;
+        currentRow++;
+        if( currentRow>= 64)
+        {
+            currentRow = 0;
+            currentOrder++;
+        }
+        else
+        {
+            updateEffects();
+        }        
+    }    
+}
+
+void Mod::updateRow() 
+{
+    //std::unique_lock<std::mutex> lk(cs);
+
+    int patternIdx = order[currentOrder];
+    printf("updaterow... ptn=%d, ord=%d, row=%d\n", patternIdx, currentOrder, currentRow );
+    for(int channelIdx = 0; channelIdx < nChannels; ++channelIdx)
+    {        
+        Note& note = patternData[(64*nChannels*patternIdx)+(nChannels*currentRow)+channelIdx];
+        Channel& channel = channels[channelIdx];
+        if( note.sampleNumber )
+        {
+            Sample* smpl = &samples[note.sampleNumber-1];
+            channel.lastSample = smpl;
+            channel.setVol(smpl->volume);
+            //printf( "chn %d, set sample %d '%s'\n", channelIdx, note.sampleNumber, smpl->name.c_str());  
+        }
+        if( note.noteOffset >=0 ) //=0 is fail.. hmm...
+        {
+           if (note.effect != 3 && note.effect != 5) 
+           {
+                if( channel.lastSample )
+                    channel.ft = channel.lastSample->fine_tune;
+
+                channel.freqOffset = note.noteOffset + (12*3*channel.ft);
+           }
+        }
+
+        // skip effects.
+        if( note.effect !=0 && note.eparm != 0)
+        {
+            // tick sfx.
+        }
+
+        if( channel.freqOffset > 0 )
+            channel.setFreq(channel.freqOffset);
+        
+        if(note.noteOffset)
+        {
+            channel.play();
+        }
+
+        //if( channelIdx == 0) break; // Testing
+    }
+}
+
+size_t Mod::makeAudio(RingBuffer<float>* stereoRingBuffer, float sampleRate, int frameSize)
+{
+    //std::unique_lock<std::mutex> lk(cs);
+
+    //size_t toWrite = Min(stereoRingBuffer->space()/2, frameSize);
+
+    std::vector<float> mixBuffer;
+    mixBuffer.resize(frameSize);
+    for(int i = 0; i < nChannels; ++i)
+    {
+        Channel& c = channels[i];
+        size_t made = c.makeAudio(mixBuffer.data(), frameSize, sampleRate);
+        //if( i== 0) break; // testing.
+    }
+
+    float mix = 0;
+    float gain = (float)1.f / nChannels;
+    int i = 0;
+    for(; i < frameSize; ++i)
+    {
+        float val = mixBuffer[i] * gain;
+        if(!stereoRingBuffer->push(val)) break;
+        if(!stereoRingBuffer->push(val)) break;
+    }
+    return i;
+}
 
 void testCircbuffer()
 {
@@ -76,7 +290,8 @@ void testCircbuffer()
         assert(a.num() == 0);
         a.push(0.12345f); 
         assert(a.num() == 1);
-        float tmp = a.pop();
+        float tmp;
+        assert(a.pop(tmp));
         assert(tmp == 0.12345f);
     }
     // test 2. write a bunch.
@@ -87,7 +302,8 @@ void testCircbuffer()
             assert(a.num() == 0);
             a.push(0.12345f);
             assert(a.num() == 1);
-            float tmp = a.pop();
+            float tmp;
+            assert(a.pop(tmp));
             assert(tmp == 0.12345f);
         } 
     }
@@ -95,13 +311,15 @@ void testCircbuffer()
     // test 3. 
     {
         RingBuffer<float> a(128);
-        while(a.space())
+        while(a.space()>0)
         {
             a.push(0.12345f); 
         } 
         while(a.num())
         {
-            float tmp = a.pop();
+            assert(a.num() > 0);
+            float tmp;
+            assert(a.pop(tmp));
             assert(tmp == 0.12345f); 
         }
     }
@@ -111,22 +329,56 @@ void testCircbuffer()
         RingBuffer<float> a(128);
         for( int i = 0; i < 64; ++i )
         {
-            a.push((float)i);    
             assert(a.num() == i);
+            a.push((float)i);    
         }
         for( int i=0; i < 64; ++i)
         {
-            float tmp = a.pop();
+            float tmp;
+            assert(a.num() == 64-i);
+            assert(a.pop(tmp));
             assert(tmp == (float)i); 
+        }
+    }
+
+     // test 5. 
+    {
+        RingBuffer<float> a(128);
+        assert(a.space() == 127);
+        for( int i = 0; i < 128; ++i )
+        {
+            assert(a.num() == i);
+            a.push((float)i);    
+        }
+        assert(a.space()==0);
+        assert(a.isFull());
+        assert(!a.isEmpty());
+
+        for( int i=0; i < 128; ++i)
+        {
+            float tmp;
+            assert(a.pop(tmp) || i == 127);
+            assert(tmp == (float)i || i == 127); 
         }
     }
 }
 
-static std::atomic<bool> gExit;
-static std::atomic<bool> gWake;
-static std::condition_variable gWakeCv;
-static std::mutex m;
-RingBuffer<float> gOutput(1024*2);
+
+void tickThread()
+{
+    printf("Starting tick thread.\n");
+    while(!gExit)
+    {
+        auto start = std::chrono::system_clock::now();
+        auto end = start + std::chrono::milliseconds(20); // 50hz
+        while(std::chrono::system_clock::now() < end)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(0));
+            //gMod.tick();
+        }
+        gMod.tick();
+    }
+}
 
 void producerThread()
 {
@@ -136,52 +388,40 @@ void producerThread()
     int samplePos = 0;
     float leftPhase = 0.0f;
     float rightPhase = 0.0f;
+    int ft = 0, semi, oct;
+    int noteOff = findNoteFromFreq(215,semi, oct, ft);
+    auto lastTickTime = std::chrono::system_clock::now();
     while(!gExit)
     {
+        auto loopStart = std::chrono::system_clock::now();
         {
             std::unique_lock<std::mutex> lk(m);
-            auto now = std::chrono::system_clock::now();
-            if( !gWakeCv.wait_until(lk,now + std::chrono::milliseconds(200), []() -> bool { return gWake; } ) )
-                printf("Wait timeout...\n");
+            if( !gWakeCv.wait_until(lk,loopStart + std::chrono::milliseconds(10), []() -> bool { return gWake; } ) )
+            {
+                //printf("Wait timeout...\n");
+            }
         }
+
         //printf("Waking up...\n");
         if(gMod.samples.size() < 31) continue;
 
         int samplesProduced = 0;
-        while(gOutput.space() > 0)
+        while(samplesProduced < kFrameSize && !gOutput.isFull())
         {
             Sample& s = gMod.samples[currentSample];
-
-            for( ; samplePos < s.length; ++samplePos)
-            {
-                float val = (float)(s.data[samplePos] / 256.f);
-                if( !gOutput.push(val))
-                    break;
-                if( !gOutput.push(val))
-                    break;
-                samplesProduced+=2;
-            }
-            if( samplePos >= s.length)
-            {
-                samplePos = 0;
-                currentSample = (currentSample +1) % 31;
-                printf("sample=%d\n", currentSample);
-            }
+            bool bLooping = s.loop_length > 0;
+            
+            samplesProduced += gMod.makeAudio(&gOutput, SAMPLE_RATE, kFrameSize );
         }
-
-        // produce some samples...
-        /*
-        for( int i = 0; i < 1024; ++i)
-        {
-            leftPhase += 0.03f;
-            if( leftPhase >= 1.0f ) leftPhase -= 2.0f;
-            rightPhase += 0.01f;
-            if( rightPhase >= 1.0f ) rightPhase -= 2.0f;
-                
-            if(!gOutput.push(leftPhase)) break;
-            if(!gOutput.push(rightPhase)) break;
-        }*/
         gWake = false;
+
+        // Tick if we need to.
+        auto tickDelta =std::chrono::system_clock::now() - lastTickTime;
+        if( tickDelta > std::chrono::milliseconds(20) )
+        {
+            lastTickTime = std::chrono::system_clock::now();
+            gMod.tick();    
+        }
     }
 
     printf("Exiting worker thread.\n");
@@ -210,8 +450,8 @@ static int patestCallback( const void *inputBuffer, void *outputBuffer,
     {
         if( buff->num() >= 2)
         {
-            *out++ = buff->pop();
-            *out++ = buff->pop(); 
+            buff->pop(*out++);
+            buff->pop(*out++); 
         }
         else
         {
@@ -231,10 +471,6 @@ static int patestCallback( const void *inputBuffer, void *outputBuffer,
     gWakeCv.notify_all();
     return 0;
 }
-
-#define ERR_WRAP(mac_err) do { result = mac_err ; line = __LINE__ ; if ( result != paNoError ) goto error; } while(0)
-#define SAMPLE_RATE (22000)
-#define NUM_SECONDS (10)
 
 
 int getChannelCount(const void* mem, size_t size)
@@ -260,6 +496,8 @@ bool loadMod(void* mem, size_t size)
     int nChannels = getChannelCount(mem,size);
     if(!nChannels) return false; // valid?
     gMod.nChannels = nChannels;
+    for(int i = 0; i < gMod.nChannels; i++)
+        gMod.channels.push_back(std::move(Channel()));
 
     // name.    
     char name[21] = {0};
@@ -299,13 +537,17 @@ bool loadMod(void* mem, size_t size)
         sampleInfoState+=2;
 
         // loop length
-        packed = *(uint16_t*)sampleInfoState;   
-        s.loop_length = (((packed & 0xff) * 0x100) + (packed>>8)) * 2;
+        packed = *(uint16_t*)sampleInfoState; 
+        uint8_t byte1 = (packed >> 0) & 0xff;
+        uint8_t byte2 = (packed >> 8) & 0xff;
+        s.loop_length = ((int)(byte1*0x100) + byte2);
+        if( s.loop_length == 1) s.loop_length = 0; // 1 means non-looping.
+        else s.loop_length *= 2;
         sampleInfoState+=2; 
         
         if( s.length )
         {
-            printf("%d, name='%s', length=0x%x, finetune=%d, vol=%d, loopstart=%d, looplength=%d\n", 
+            printf("%d, name='%s', length=0x%x, finetune=%d, vol=%d, loopstart=%x, looplength=%x\n", 
                 i, name, (int)s.length, s.fine_tune, s.volume, s.loop_start, s.loop_length);
         }
         gMod.samples.push_back(std::move(s));
@@ -342,22 +584,25 @@ bool loadMod(void* mem, size_t size)
         for(int j = 0; j < gMod.nChannels * 64; j++)
         {
             uint32_t packed = *patterns++;
-            //store SAMPLE_NUMBER as    (byte0 AND 0F0h) + (byte2 SHR 4)
+
             uint8_t byte0 = packed & 0xff;
+            uint8_t byte1 = (packed >> 8)  & 0xff;
             uint8_t byte2 = (packed >> 16) & 0xff;
-            note->number = (byte0 & 0xf0) + (byte2 >> 4);
+            uint8_t byte3 = (packed >> 24) & 0xff;
+
+            //store SAMPLE_NUMBER as    (byte0 AND 0F0h) + (byte2 SHR 4)
+            note->sampleNumber = (byte0 & 0xf0) + (byte2 >> 4);
 
             //- store PERIOD_FREQUENCY as ((byte0 AND 0Fh) SHL 8) + byte1;
-            uint32_t freq = ((packed & 0xf) << 8) + ((packed >> 8) & 0xff);
+            uint32_t freq = ((byte0 & 0x0f) << 8) + byte1;
             int oct, semi, ft;
-            note->note = findNoteFromFreq(freq, semi, oct, ft);
+            note->noteOffset = findNoteFromFreq(freq, semi, oct, ft);
             
             //- store EFFECT_NUMBER as    byte2 AND 0Fh
-            note->effect = (packed >> 16) & 0xf;
+            note->effect = byte2 & 0xf;
 
             //- store EFFECT_PARAMETER as byte 3
-            note->eparm = (packed >> 24) & 0xff;
-            const char* notestr[] = {"C-", "C#", "D-", "D#","E-", "F-", "F#", "G-","G#","A-", "A#", "B-" };
+            note->eparm =  byte3;
 
             if( j % gMod.nChannels == 0 )
             {
@@ -365,10 +610,10 @@ bool loadMod(void* mem, size_t size)
                 printf("%02x    ", j / 4);
             }
             // note.
-            if( note->note) printf("%s%d ", notestr[semi],oct);
-            else printf("... ");
+            if( note->noteOffset) printf("%s%d %d ", notestr[semi],oct,ft);
+            else printf("..... ");
             // sample #
-            if( note->number ) printf("%02x ", note->number);
+            if( note->sampleNumber ) printf("%02x ", note->sampleNumber);
             else printf(".. ");
             // effect.
             printf("%x%02x", note->effect, note->eparm);
@@ -413,6 +658,7 @@ bool loadMod(const char* filename)
 
 int main(int argc, char** argv)
 {
+    testCircbuffer();
     if( argc <= 1 )
     {
         printf("please supply a mod filename.\n");
@@ -423,6 +669,9 @@ int main(int argc, char** argv)
         printf("failed to load mod file '%s'\n", argv[1]);
         return -1;
     }
+
+    //std::thread t(tickThread);
+    //t.detach();
 
     std::thread r(producerThread);
     r.detach();
@@ -445,6 +694,7 @@ int main(int argc, char** argv)
                                 2,          /* stereo output */
                                 paFloat32,  /* 32 bit floating point output */
                                 SAMPLE_RATE,
+                                //256,
                                 paFramesPerBufferUnspecified,        
                                                     /* frames per buffer, i.e. the number
                                                    of sample frames that PortAudio will
@@ -459,7 +709,7 @@ int main(int argc, char** argv)
 
     ERR_WRAP(Pa_StartStream( stream ));
 
-    Pa_Sleep(NUM_SECONDS*1000);
+   Pa_Sleep(NUM_SECONDS*100000);
 
     ERR_WRAP(Pa_StopStream( stream ));
 
@@ -485,7 +735,7 @@ error:
 }
 
 //
-static uint16_t gNotes[12*3*16] = 
+const uint16_t gNotes[12*3*16] = 
     {
         // Tuning -8
         907,856,808,762,720,678,640,604,570,538,508,480
